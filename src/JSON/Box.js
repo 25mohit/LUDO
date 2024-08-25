73de2de24b754bcaf0e76a37d29f252045742923
25mohit <mohit724agarwal@gmail.com>
2024-08-25 23:58:32 +0530

    D

--- src/JSON/Box.js
+++ src/JSON/Box.js
@@ -7,15 +7,11 @@ const tTypes = {
     26: 's',
     36: 's',
     41: 's',
-    49: 's',
-    35: 'danger',
-    8: 'warning',
-    24: 'alert',
-    // Add more positions and their corresponding 't' types as needed
+    49: 's'
 };
 
 export const boxes = Array.from({ length: 52 }, (_, i) => ({
     n: `b${i + 1}`,
     s: (i == 1 || i == 14 || i == 27 || i == 40) ? 'start' : '',
-    t: tTypes[i + 1] || 'p', // Default to 'safe' if the position is not specified in tTypes
+    t: tTypes[i + 1] || 'p',
 }));
